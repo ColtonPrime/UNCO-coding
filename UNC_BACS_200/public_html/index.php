b039c31563789fcd894e5829fc01ba69de887d73
ColtonPrime <50209755+ColtonPrime@users.noreply.github.com>
2019-12-04 13:07:02 -0700

    personal blog

---
+++ UNC_BACS_200/public_html/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>__TITLE__</title>
+    <!--page title-->
+    <link rel="stylesheet" href="template.css">
+    <!--link to css stylesheet-->
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li>Menu Item 1</li>
+            <!--Menu item 1-->
+            <li>Menu Item 2</li>
+            <!--Menu item 2-->
+        </ul>
+    </nav>
+    <header>
+        <img id="titlepic" src="pandaface.png" alt="Bear Logo">
+        <!--Picture/image-->
+        <h1>__TITLE__</h1>
+        <!--main title-->
+    </header>
+
+    <main>
+        <h2>__HEADLINE__</h2>
+        <!--first sub headline-->
+
+        <p>Paragraph #1</p>
+        <!--paragraph-->
+        <img src="pandaface.png" alt="Bear Logo">
+        <p>Paragraph #2</p>
+        <!--paragraph-->
+        <img src="pandaface.png" alt="Bear Logo">
+        <ul>
+            <li>__ITEM__</li>
+            <!--item-->
+            <li>__ITEM__</li>
+            <!--item-->
+        </ul>
+    </main>
+
+    <footer>
+        <p>&copy; Copyright 2018 - Me, MySelf, and I</p>
+        <!--relavent legal information-->
+    </footer>
+
+</body>
+
+</html>
